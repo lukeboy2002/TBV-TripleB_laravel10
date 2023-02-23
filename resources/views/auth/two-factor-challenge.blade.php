a0cdd3222596178ba765ac97a5bceb89dc2cd60e
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-02-23 20:22:01 +0100

    changed login with 2fa
    Also moved unused components

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -1,57 +1,55 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
+<x-app-layout>
+    <x-cards.small>
         <div x-data="{ recovery: false }">
-            <div class="mb-4 text-sm text-gray-600" x-show="! recovery">
-                {{ __('Please confirm access to your account by entering the authentication code provided by your authenticator application.') }}
+            <div class="mb-4 text-sm text-gray-700 dark:text-white" x-show="! recovery">
+                Please confirm access to your account by entering the authentication code provided by your authenticator application.
             </div>
 
-            <div class="mb-4 text-sm text-gray-600" x-show="recovery">
-                {{ __('Please confirm access to your account by entering one of your emergency recovery codes.') }}
+            <div class="mb-4 text-sm text-gray-700 dark:text-white" x-show="recovery">
+                Please confirm access to your account by entering one of your emergency recovery codes.
             </div>
 
             <x-validation-errors class="mb-4" />
 
             <form method="POST" action="{{ route('two-factor.login') }}">
                 @csrf
-
                 <div class="mt-4" x-show="! recovery">
-                    <x-label for="code" value="{{ __('Code') }}" />
-                    <x-input id="code" class="block mt-1 w-full" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
+                    <x-form.label for="code" value="{{ __('Code') }}" />
+                    <x-form.input id="code" class="block mt-1 w-full" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
+                    <x-form.input-error for="code" class="mt-2" />
+
                 </div>
 
                 <div class="mt-4" x-show="recovery">
-                    <x-label for="recovery_code" value="{{ __('Recovery Code') }}" />
-                    <x-input id="recovery_code" class="block mt-1 w-full" type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
+                    <x-form.label for="recovery_code" value="{{ __('Recovery Code') }}" />
+                    <x-form.input id="recovery_code" class="block mt-1 w-full" type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
+                    <x-form.input-error for="recovery_code" class="mt-2" />
                 </div>
 
                 <div class="flex items-center justify-end mt-4">
-                    <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
+                    <x-buttons.secondary type="button" class="px-3 py-2 text-xs font-medium"
                                     x-show="! recovery"
                                     x-on:click="
                                         recovery = true;
                                         $nextTick(() => { $refs.recovery_code.focus() })
                                     ">
                         {{ __('Use a recovery code') }}
-                    </button>
+                    </x-buttons.secondary>
 
-                    <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
+                    <x-buttons.secondary type="button" class="px-3 py-2 text-xs font-medium"
                                     x-show="recovery"
                                     x-on:click="
                                         recovery = false;
                                         $nextTick(() => { $refs.code.focus() })
                                     ">
                         {{ __('Use an authentication code') }}
-                    </button>
+                    </x-buttons.secondary>
 
-                    <x-button class="ml-4">
+                    <x-buttons.primair class="ml-4 px-3 py-2 text-xs font-medium">
                         {{ __('Log in') }}
-                    </x-button>
+                    </x-buttons.primair>
                 </div>
             </form>
         </div>
-    </x-authentication-card>
-</x-guest-layout>
+    </x-cards.small>
+</x-app-layout>
