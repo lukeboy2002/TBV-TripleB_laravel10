cfee424c0978bca0efd117e1ce13ef14226e1faf
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-03-04 17:03:22 +0100

    Removed sponsors add and edit

--- resources/views/admin/sponsors/create.blade.php
+++
@@ -1,9 +0,0 @@
-<x-admin-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-orange-500 leading-tight">
-            TBV-TripleB Create New Sponsor <i class="fa-solid fa-hand-holding-dollar mr-4"></i>
-        </h2>
-    </x-slot>
-
-    <livewire:admin.sponsors.sponsors-form />
-</x-admin-layout>
