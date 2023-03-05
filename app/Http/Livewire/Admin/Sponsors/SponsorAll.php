2ffcdb8772fd604c70e99925da7677079e8810ce
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-03-05 16:18:23 +0100

    Created files for Sponsors

---
+++ app/Http/Livewire/Admin/Sponsors/SponsorAll.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Sponsors;
+
+use Livewire\Component;
+
+class SponsorAll extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.sponsors.sponsor-all');
+    }
+}
